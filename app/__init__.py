c9b4c8355eca9ac52308de85e4f81133b3220a51
Nobella-Nyarari-Ejiofor <nobella.ejiofor@student.moringaschool.com>
2022-01-30 18:14:40 -0500

    Added a configuration for the debug process

--- app/__init__.py
+++ app/__init__.py
@@ -4,9 +4,10 @@ from .config import DevConfig
 
 
 #Initialising flask application
-app = Flask(__name__)
+app = Flask(__name__,instance_relative_config = True)
 
 # Setting up configuration
-app.config.from_object(DevConfig)
+app.config.from_pyfile('config.py')
+
 
 from app import views
