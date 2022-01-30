565b75a48f4c6c7d32f3fd04e828d44554722a14
Nobella-Nyarari-Ejiofor <nobella.ejiofor@student.moringaschool.com>
2022-01-30 18:13:51 -0500

    Added the base url and api key

--- app/request.py
+++ app/request.py
@@ -1,3 +1,7 @@
-from newsapi import NewsApiClient
+from app import app
+import urllib.request,json
+from models import news
+
+News = news.News
 
 newsapi =f' https://newsapi.org/v2/top-headlines?country=us&apiKey=11319835f3f642b08ffc5ed98495e990'
--- config.py
+++ config.py
@@ -1,1 +1,31 @@
-import os
+class Config:
+    '''
+    General configuration parent class
+    '''
+    pass
+
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
+
+
+API_KEY = '11319835f3f642b08ffc5ed98495e990'
+BASE_URL = 'https://newsapi.org/v2/{}?q=Apple&from=2022-01-25&sortBy=popularity&apiKey=11319835f3f642b08ffc5ed98495e990'
